39dcba1c83592feccd75c2db5a0a64a1973ed80d
Dwayne Crooks <me@dwaynecrooks.com>
2024-05-20 07:06:54 -0400

    Initial commit
    Implement a parser for L_Int.

---
+++ tests/Test/Ch1/L_Int/Parser.elm
@@ -0,0 +1,57 @@
+module Test.Ch1.L_Int.Parser exposing (suite)
+
+import Ch1.L_Int.AST exposing (..)
+import Ch1.L_Int.Parser as P
+import Expect
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "Ch1.L_Int.Parser"
+        [ parseSuite
+        ]
+
+
+parseSuite : Test
+parseSuite =
+    describe "parse" <|
+        List.map testParse
+            [ ( "-5", Int -5 )
+            , ( "0", Int 0 )
+            , ( "5", Int 5 )
+            , ( "(read)", Prim Read )
+            , ( " ( read ) ", Prim Read )
+            , ( "(-5)", Prim (Negate (Int 5)) )
+            , ( "(- 5)", Prim (Negate (Int 5)) )
+            , ( "(- 8 6)", Prim (Sub (Int 8) (Int 6)) )
+            , ( "(+ 15 9)", Prim (Add (Int 15) (Int 9)) )
+            , ( "(+ 8 -6)", Prim (Add (Int 8) (Int -6)) )
+
+            --
+            -- From (1.1)
+            --
+            , ( "(+ (read) (- 8))"
+              , Prim (Add (Prim Read) (Prim (Negate (Int 8))))
+              )
+            ]
+
+
+testParse : ( String, Expr ) -> Test
+testParse ( input, expectedExpr ) =
+    test input <|
+        \_ ->
+            case P.parse input of
+                Ok actualProgram ->
+                    let
+                        expectedProgram =
+                            Program expectedExpr
+                    in
+                    if expectedProgram == actualProgram then
+                        Expect.pass
+
+                    else
+                        Expect.fail <| "expected = " ++ Debug.toString expectedProgram ++ ", actual = " ++ Debug.toString actualProgram
+
+                Err e ->
+                    Expect.fail <| Debug.toString e
