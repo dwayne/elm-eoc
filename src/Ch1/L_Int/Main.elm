0db6fd06228c173dce878a30ea516451ec77acf0
Dwayne Crooks <me@dwaynecrooks.com>
2024-05-21 20:53:55 -0400

    Start exploring what the UI could look like

--- src/Ch1/L_Int/Main.elm
+++ src/Ch1/L_Int/Main.elm
@@ -3,6 +3,7 @@ module Ch1.L_Int.Main exposing (main)
 
 import Browser
 import Html as H
+import Html.Attributes as HA
 
 
 main : Program () Model msg
@@ -39,4 +40,48 @@ update =
 
 view : Model -> H.Html msg
 view _ =
-    H.text "Hello, world!"
+    H.div []
+        [ H.textarea
+            [ HA.rows 20
+            , HA.cols 80
+            , HA.placeholder "(+ (read) (- 8))"
+            ]
+            []
+        , H.p [] [ H.button [] [ H.text "Run" ] ]
+        , H.div
+            [ HA.class "console" ]
+            [ viewConsoleLine "12"
+            , viewConsoleLine "01234567890123456789012345678901234567890123456789012345678901234567890123456789"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            , viewConsoleLine "12"
+            ]
+        ]
+
+viewConsoleLine : String -> H.Html msg
+viewConsoleLine text =
+    H.div [ HA.class "console__line" ] [ H.text text ]
